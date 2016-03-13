99b59d8d9ed5ab872313d9ce61bc892b5500ff5f
Anisan <eraser1981@gmail.com>
2016-03-13 15:11:53 +0300

    init module

---
+++ modules/ivona_tts/IvonaTTS.php
@@ -0,0 +1,110 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////////////////
+//   IVONA_TTS             ////////////////////////////////////////////////////////////
+//    by Titus 15.10.2015  ////////////////////////////////////////////////////////////
+//    enhanced by Thorsten Kugelberg 30.10.2015 ///////////////////////////////////////
+//    31.12.2015: removed diry hack for hash    ///////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+class IvonaTTS{
+    private $utc_tz      = "";
+    private $access_key  = "";
+    private $secret_key  = "";
+    private $language    = "";
+    private $voice       = "";
+    private $rate        = "";
+    private $volume      = "";
+    public function __construct( $access_key, $secret_key , $language="de-DE", $voice="Marlene", $rate="medium", $volume="loud"){
+        $this->utc_tz      = new \DateTimeZone( 'UTC' );
+        $this->access_key  = $access_key;
+        $this->secret_key  = $secret_key;
+        $this->language    = $language;
+        $this->voice       = $voice;
+        $this->rate        = $rate;
+        $this->volume      = $volume;
+    }
+    public function save_mp3($text, $filename) {
+            $mp3 = $this->get_mp3($text);
+            file_put_contents($filename, $mp3);
+     }
+    public function get_mp3( $text )
+    {
+        $payload = json_encode(array( ('Input')      => array(('Data')     => $text),
+                                      ('Parameters') => array(('Rate')     => $this->rate,
+                                                              ('Volume')   => $this->volume),
+                                      ('Voice')      => array(('Name')     => $this->voice,
+                                                              ('Language') => $this->language )));
+       
+        $datestamp                = new \DateTime( "now", $this->utc_tz );
+        $longdate                 = $datestamp->format( "Ymd\\THis\\Z");
+        $shortdate                = $datestamp->format( "Ymd" );
+        $ksecret                  = 'AWS4' . $this->secret_key;
+        $params                   = array( 'host'                 => 'tts.eu-west-1.ivonacloud.com',
+                                           'content-type'         => 'application/json',
+                                           'x-amz-content-sha256' => hash( 'sha256', $payload, false ),
+                                           'x-amz-date'           => $longdate );
+        $canonical_request        = $this->createCanonicalRequest( $params, $payload );
+        $signed_request           = hash( 'sha256', $canonical_request, false );
+        $sign_string              = "AWS4-HMAC-SHA256\n{$longdate}\n$shortdate/eu-west-1/tts/aws4_request\n" . $signed_request;
+        $signature                = hash_hmac( 'sha256', $sign_string, hash_hmac( 'sha256', 'aws4_request', hash_hmac( 'sha256', 'tts', hash_hmac( 'sha256', 'eu-west-1', hash_hmac( 'sha256', $shortdate, $ksecret, true ) , true ) , true ), true ));
+        $params['Authorization']  = "AWS4-HMAC-SHA256 Credential=" . $this->access_key . "/$shortdate/eu-west-1/tts/aws4_request, " .
+                                    "SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, " .
+                                    "Signature=$signature";
+        $params['content-length'] = strlen( $payload ) ;
+        /*
+         * Execute Crafted Request
+         */
+        $url    = "https://tts.eu-west-1.ivonacloud.com/CreateSpeech";
+        $ch     = curl_init();
+        $curl_headers = array();
+        foreach( $params as $p => $k )
+            $curl_headers[] = $p . ": " . $k;
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_POST,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_TCP_NODELAY, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false );
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false );
+        // debug opts
+        {
+            curl_setopt($ch, CURLOPT_VERBOSE, true);
+            $verbose = fopen('php://temp', 'rw+');
+            curl_setopt($ch, CURLOPT_STDERR, $verbose);
+            $result = curl_exec($ch); // raw result
+            rewind($verbose);
+            $verboseLog = stream_get_contents($verbose);
+            //echo "Verbose information:\n<pre>", htmlspecialchars($verboseLog), "</pre>\n";
+        }
+        
+        // check for magic number of mp3 with ID3 tag
+        if ( substr(bin2hex($result), 0, 6) != "494433" )
+          throw new Exception("Response from Ivona is no mp3: ".$result); 
+        
+        return $result;
+    }
+    
+    private function createCanonicalRequest( Array $params, $payload )
+    {
+        $canonical_request      = array();
+        $canonical_request[]    = 'POST';
+        $canonical_request[]    = '/CreateSpeech';
+        $canonical_request[]    = '';
+        $can_headers            = array(
+          'host' => 'tts.eu-west-1.ivonacloud.com'
+        );
+        foreach( $params as $k => $v )
+            $can_headers[ strtolower( $k ) ] = trim( $v );
+        uksort( $can_headers, 'strcmp' );
+        foreach ( $can_headers as $k => $v )
+            $canonical_request[] = $k . ':' . $v;
+        $canonical_request[] = '';
+        $canonical_request[] = implode( ';', array_keys( $can_headers ) );
+        $canonical_request[] = hash( 'sha256', $payload, false );
+        $canonical_request = implode( "\n", $canonical_request );
+        return $canonical_request;
+    }
+} 
+?>
---
+++ modules/ivona_tts/ivona_tts.class.php
@@ -0,0 +1,182 @@
+<?php
+/**
+* Ivona TTS 
+* @package project
+* @author Wizard <sergejey@gmail.com>
+* @copyright http://majordomo.smartliving.ru/ (c)
+* @version 0.1 (wizard, 13:03:10 [Mar 13, 2016])
+*/
+//
+//
+class ivona_tts extends module {
+/**
+* ivona_tts
+*
+* Module class constructor
+*
+* @access private
+*/
+function ivona_tts() {
+  $this->name="ivona_tts";
+  $this->title="Ivona TTS";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=0) {
+ $p=array();
+ if (IsSet($this->id)) {
+  $p["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $p["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $p["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $p["tab"]=$this->tab;
+ }
+ return parent::saveParams($p);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams() {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  $out=array();
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+ $this->getConfig();
+ $out['ACCESS_KEY']=$this->config['ACCESS_KEY'];
+ $out['SECRET_KEY']=$this->config['SECRET_KEY'];
+ if ($this->view_mode=='update_settings') {
+   global $access_key;
+   $this->config['ACCESS_KEY']=$access_key;
+   global $secret_key;
+   $this->config['SECRET_KEY']=$secret_key;
+   $this->saveConfig();
+   $this->redirect("?");
+ }
+}
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ $this->admin($out);
+}
+ function processSubscription($event, $details='') {
+  $this->getConfig();
+  print_r($details);
+  if ($event=='SAY') {
+    /* Хук на функцию say() */
+    $level=$details['level'];
+    $message=$details['message'];
+    
+    include_once("./modules/ivona_tts/IvonaTTS.php");
+ 
+    $accessKey=$this->config['ACCESS_KEY'];
+    $secretKey=$this->config['SECRET_KEY'];
+    
+    if ($level >= (int)getGlobal('minMsgLevel'))
+    {
+        $filename       = md5($message) . '_ivona.mp3';
+        $cachedVoiceDir = ROOT . 'cached/voice';
+        $cachedFileName = $cachedVoiceDir . '/' . $filename;
+        
+        if (!file_exists($cachedFileName))
+        {
+            $obj = new IvonaTTS($accessKey,$secretKey,"ru-RU","Tatyana"); 
+            $obj->save_mp3($message,$cachedFileName);
+        }
+        @touch($cachedFileName);
+        playSound($cachedFileName, 1, $level);
+    }
+  }
+ }
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($data='') {
+  subscribeToEvent($this->name, 'SAY');
+  parent::install();
+ }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgTWFyIDEzLCAyMDE2IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
